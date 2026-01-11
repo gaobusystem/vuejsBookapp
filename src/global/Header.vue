4f47f5b599ba3f6373a94499ffd5b508d88d15c8
yamato2323 <dev@gaobusystem.com>
2026-01-12 01:14:47 +0900

    この Header.vue は Vue2 の Options API + Vuetify2 の書き方が混ざっているため、Vue3 + Vuetify3 では内部的に slot が正しく扱われず、あの警告が出ています。

--- src/global/Header.vue
+++ src/global/Header.vue
@@ -1,71 +1,73 @@
 <template>
-  <v-app-bar
-    color="gray accent-4"
-    dark
-  >
-  <v-btn icon color="primary" to="/">
-    <v-icon>mdi-home</v-icon>
-  </v-btn>
-  <v-toolbar-title>漫画管理</v-toolbar-title>
-  <div class="flex-grow-1"></div>
+  <v-app-bar color="gray accent-4" dark>
 
-  <v-btn icon>
-    <v-icon>mdi-heart</v-icon>
-  </v-btn>
-
-  <v-btn icon color="primary" to="/search">
-    <v-icon>mdi-magnify</v-icon>
-  </v-btn>
-  <div class="d-flex justify-space-around">
-
-    <v-btn
-      id="menu-activator"
-      color="primary"
-
-    >
-
-            <svg-icon type="mdi" :path="path"></svg-icon>
-
-
+    <!-- ホーム -->
+    <v-btn icon color="primary" to="/">
+      <v-icon>mdi-home</v-icon>
     </v-btn>
 
-    <v-menu activator="#menu-activator">
+    <v-toolbar-title>漫画管理</v-toolbar-title>
+
+    <div class="flex-grow-1"></div>
+
+    <!-- ハート -->
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <!-- 検索 -->
+    <v-btn icon color="primary" to="/search">
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <!-- メニュー -->
+    <v-menu>
+      <template #activator="{ props }">
+        <v-btn v-bind="props" icon color="primary">
+          <SvgIcon type="mdi" :path="path" />
+        </v-btn>
+      </template>
+
       <v-list>
         <v-list-item
           v-for="(item, index) in items"
           :key="index"
-          :value="index"
+          @click="onMenuClick(index)"
         >
           <v-list-item-title>{{ item.title }}</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-menu>
-  </div>
+
   </v-app-bar>
 </template>
 
-<script>
-import SvgIcon from '@jamescoyle/vue-icon';
+<script setup>
+import { ref } from 'vue'
+import { SvgIcon } from '@jamescoyle/vue-icon'
 import { mdiMenu } from '@mdi/js'
-export default {
-  name:'Header',
-  components: { SvgIcon },
 
-  methods:{
-    deleteLocalStorage(){
-      this.$emit('delete-local-storage')
-    }
-  },
-  data: () => ({
-    path: mdiMenu,
-    items: [
-      { title: 'Click Me' },
-      { title: 'Click Me' },
-      { title: '初期データインポート' },
-      { title: '登録データをクリア' },
-    ],
-  }),
+// メニューアイコン
+const path = mdiMenu
+
+// メニュー項目
+const items = ref([
+  { title: 'Click Me' },
+  { title: 'Click Me' },
+  { title: '初期データインポート' },
+  { title: '登録データをクリア' },
+])
+
+// emit
+const emit = defineEmits(['delete-local-storage'])
+
+// メニュークリック処理
+function onMenuClick(index) {
+  if (items.value[index].title === '登録データをクリア') {
+    emit('delete-local-storage')
+  }
 }
 </script>
 
-<style></style>
+<style scoped>
+</style>
