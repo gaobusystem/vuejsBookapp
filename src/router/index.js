ab56dd39df3a7c9c5d65df3ac2a55517781d92e1
yamato2323 <dev@gaobusystem.com>
2026-01-25 21:59:04 +0900

    [router]スクロールテク

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,25 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+  console.log(to, from, savedPosition)
+  return new Promise((resolve) => {
+      setTimeout(() => {
+        if (savedPosition) {
+          return resolve(savedPosition)
+        }
+        if (to.hash) {
+          return resolve({
+            el: to.hash,
+            top: 20,
+            behavior: 'smooth'
+          })
+        }
+        return resolve({ top: 0, left: 0, behavior: 'smooth' })
+      }, 2000)
+    })
+  },
 })
 
 export default router
