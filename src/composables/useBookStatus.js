bed9db5d1f03418d62d09db28863cbb238f746dd
yamato2323 <dev@gaobusystem.com>
2026-02-04 20:25:57 +0900

    未読の値変更

--- src/composables/useBookStatus.js
+++ src/composables/useBookStatus.js
@@ -2,8 +2,8 @@ export function useBookStatus() {
   const statusList = [
     { code: 0, label: '未処理' },
     { code: 2, label: '新刊発売' },
-    { code: 4, label: '未読' },
     { code: 5, label: '読書中' },
+    { code: 6, label: '未読' },
     { code: 8, label: '未発売' },
     { code: 90, label: '完読' },
   ]
